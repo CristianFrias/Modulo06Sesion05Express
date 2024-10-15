9451ccf1faa745f4c58d300a415d66f1c829263b
Cristián Andrés Frías García <cristianfrias@MacBook-Air-de-Cristian.local>
2024-10-14 21:48:38 -0300

    Introducción API REST

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 // Creamos const app
 const express = require("express")
+const axios = require("axios")
+const { readFile, readFileSync, writeFileSync} = require("fs")
 const app = express()
 
 // Levantamos un puerto
@@ -9,6 +11,9 @@ const port = 3_000
 
 // Levantamos app
 app.listen(port, () => console.log(`Aplicación ejecutándose por el puerto ${port}`))
+
+const URL_BASE_STARWARS = "https://swapi.dev/api"
+
 
 app.get("/", (request, response) => { // esa funcion es string, definimos callback
     // Objeto de respuesta
@@ -19,6 +24,40 @@ app.get("/", (request, response) => { // esa funcion es string, definimos callba
 app.get("/curso", (req, resp) => { // primer parametro objeto de peticion y segundo parametro de respuesta
     resp.sendFile(`${__dirname}/views/curso.html`)
 })
+
+// Lunes 14 Oct
+// Definir rutas para mostrar personajes, como recibir parametros, capturar esos valores y tomarlos en la app
+app.get("/agregar-personaje", async (req, resp) => {
+    // resp.send("Agregando personaje...")
+    const idPersonaje = req.query.id_personaje
+    try {
+        const contentFile = readFileSync(`${__dirname}/files/personajes.txt`, "utf-8")
+        const contentJS = JSON.parse(contentFile)
+        const busqueda = contentJS.find(item => item.id == idPersonaje)
+
+        if (busqueda) {
+            return resp.status(409).json({message: "Personaje registrado previamente"})
+        }
+
+        const { data } = await axios.get(`${URL_BASE_STARWARS}/people/${idPersonaje}`)
+        const personaje = {id: idPersonaje, ...data}
+        contentJS.push(personaje) // se añadió el id para tn
+        writeFileSync(`${__dirname}/files/personajes.txt`, JSON.stringify(contentJS), "utf-8")
+        resp.json({message: "Personaje agregado con éxito", data: personaje})
+    } catch (error) {
+        console.log(error);
+        resp.status(500).json({message: "Hubo un error interno"})
+    }
+})
+
+app.get("/listar-personajes", (req, resp) => {
+    const contenido = readFileSync(`${__dirname}/files/personajes.txt`, "utf-8")
+    const contentJS = JSON.parse(contenido) //convierte el objeto en string
+     // contentJS.sort((a, b) => Number(b.id) - Number(a.id)) // Orden descendente
+    contentJS.sort((a, b) => Number(a.id) - Number(b.id)) // Orden ascendente
+    resp.json({"message": "Listado de personajes registrados", "data": contentJS})
+})
+
 
 // Ruta por defecto, se ejecuta cuando nos solicitan una ruta no definida
 app.get("*", (req, resp) => {
